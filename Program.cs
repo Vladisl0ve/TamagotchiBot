e131e9f2292b00fc6d5edad12cca4e21480331f0
Vladislove <vladislav.lapko@gmail.com>
2021-10-15 23:39:31 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+using Telegram.Bots.Extensions.Polling;
+
+namespace Telegram.Bots.Example
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateGlobalLoggerConfiguration();
+            Log.Information("Starting host");
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var builder = new HostBuilder()
+                  .ConfigureAppConfiguration(x =>
+                  {
+                      var conf = new ConfigurationBuilder()
+                               .SetBasePath(Directory.GetCurrentDirectory())
+                               .AddJsonFile("config.json", false, true)
+                               .Build();
+
+                      x.AddConfiguration(conf);
+                  })
+                  .UseSerilog()
+                  .ConfigureServices((context, services) =>
+                  {
+                      services.AddBotClient(context.Configuration["token"]);
+                      services.AddPolling<UpdateHandler>();
+                  });
+
+            return builder;
+        }
+        public static void CreateGlobalLoggerConfiguration()
+        {
+            Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Information()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Error)
+            .MinimumLevel.Override("System", LogEventLevel.Warning)
+            .WriteTo.Console().CreateLogger();
+        }
+    }
+}
---
+++ UpdateHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Serilog;
+using Telegram.Bots.Extensions.Polling;
+using Telegram.Bots.Requests;
+using Telegram.Bots.Types;
+
+namespace Telegram.Bots.Example
+{
+    public class UpdateHandler : IUpdateHandler
+    {
+        public Task HandleAsync(IBotClient bot, Update update, CancellationToken token)
+        {
+            Task task = update switch
+            {
+                MessageUpdate u when u.Data is TextMessage message => Echo(message),
+                _ => Task.CompletedTask
+            };
+            return task;
+
+            Task Echo(TextMessage message)
+            {
+                Log.Information($"Sending to @{message.Chat.Username}: {message.Text}");
+                return bot.HandleAsync(new SendText(message.Chat.Id, message.Text), token);
+            }
+        }
+    }
+}
